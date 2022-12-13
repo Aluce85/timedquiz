a2962bb0166fe3d3be14505bb963a02525c4a332
Alex Luce <alexluce85@gmail.com>
2022-12-13 17:04:41 -0600

    added the lets and constants for java

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let startButton = document.querySelector("#start");
+let startPage = document.querySelector("#startPage");
+let timer = document.querySelector("#timer");
+let currentTime = 60;
+let currentQuestion =0;
+let quizQuestions = document.querySelector("#questions");
+let countdown;
+const questions = []
